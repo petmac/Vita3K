6ff1894a1ee45fd192c6c64f7530927c525b05ec
Peter Mackay <mackay.pete+git@gmail.com>
2016-06-03 17:44:03 +0100

    Verify multiple Unicorn engines can map the same memory.

--- osx/AppDelegate.mm
+++ osx/AppDelegate.mm
@@ -10,6 +10,7 @@
 
 #include <unicorn/unicorn.h>
 
+#include <array>
 #include <vector>
 
 @interface AppDelegate ()
@@ -19,18 +20,25 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	uc_engine *uc = nullptr;
-	uc_open(UC_ARCH_ARM, UC_MODE_THUMB, &uc);
+    typedef std::array<uc_engine *, 2> Contexts;
+    Contexts ucs;
+    for (Contexts::iterator uc = ucs.begin(); uc != ucs.end(); ++uc)
+    {
+        uc_open(UC_ARCH_ARM, UC_MODE_THUMB, &*uc);
+    }
 	
 	const uint64 address = 4096;
 	std::vector<uint8_t> mapped(8192);
-	uc_mem_map_ptr(uc, address, mapped.size(), UC_PROT_ALL, &mapped.front());
-	
+    for (uc_engine *uc : ucs)
+    {
+        uc_mem_map_ptr(uc, address, mapped.size(), UC_PROT_ALL, &mapped.front());
+    }
+    
 	const uint8_t expected = 123;
-    mapped.front() = expected;
+    uc_mem_write(ucs[0], address, &expected, sizeof(expected));
 	
 	uint8_t actual = 231;
-	uc_mem_read(uc, address, &actual, sizeof(actual));
+	uc_mem_read(ucs[1], address, &actual, sizeof(actual));
 	
 	assert(actual == expected);
 }
