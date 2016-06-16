c15040c0a04ce9cfb73228aa6e081a4a2449925c
Peter Mackay <mackay.pete+git@gmail.com>
2016-06-16 08:16:09 +0100

    Generate nids header.

--- emulator/gen_nids.py
+++ emulator/gen_nids.py
@@ -4,4 +4,12 @@ import json
 
 with open("../vita-headers/db.json") as file:
     data = json.load(file)
-    print(data)
+    libraries = data.values()
+    with open("nids.h", "w") as header:
+        for library in libraries:
+            for module in library["modules"].values():
+                for function in module["functions"].items():
+                    name = function[0]
+                    nid = function[1]
+                    line = "NID(" + name + ", " + hex(nid) + ")\n"
+                    header.write(line)
