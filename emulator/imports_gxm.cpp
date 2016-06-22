aabf53b7f234382d55227c5a80e7005858b30ff9
Peter Mackay <mackay.pete+git@gmail.com>
2016-06-22 18:22:55 +0100

    sceGxmInitialize

---
+++ emulator/imports_gxm.cpp
@@ -0,0 +1,21 @@
+#include "import.h"
+
+struct SceGxmInitializeParams
+{
+    // This is guesswork based on Napier tutorial 3 PDF.
+    uint32_t flags = 0;
+    uint32_t displayQueueMaxPendingCount = 0;
+    Address displayQueueCallback;
+    Address displayQueueCallbackDataSize = 0;
+    uint32_t parameterBufferSize = 0;
+};
+
+IMP_SIG(sceGxmInitialize)
+{
+    const SceGxmInitializeParams *const params = mem_ptr<const SceGxmInitializeParams>(r0, mem);
+    (void)params;
+    
+    // TODO Implement.
+    // TODO Move SCE_OK to its own file.
+    return 0;
+}
