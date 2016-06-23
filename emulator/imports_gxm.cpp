94473588b47ce59573e057162d2e7188bc8bd237
Peter Mackay <mackay.pete+git@gmail.com>
2016-06-23 17:09:29 +0100

    sceGxmMapMemory

--- emulator/imports_gxm.cpp
+++ emulator/imports_gxm.cpp
@@ -1,5 +1,11 @@
 #include "import.h"
 
+enum GxmMemoryAttrib
+{
+    // https://github.com/xerpi/vitahelloworld/blob/master/draw.c
+    SCE_GXM_MEMORY_ATTRIB_RW = 3
+};
+
 struct SceGxmInitializeParams
 {
     // This is guesswork based on Napier tutorial 3 PDF.
@@ -16,5 +22,17 @@ IMP_SIG(sceGxmInitialize)
     (void)params;
     
     // TODO Implement.
+    return SCE_OK;
+}
+
+IMP_SIG(sceGxmMapMemory)
+{
+    const void *const address = mem_ptr<const void>(r0, mem);
+    const size_t size = r1;
+    const GxmMemoryAttrib attributes = static_cast<GxmMemoryAttrib>(r2);
+    assert(address != nullptr);
+    assert(size > 0);
+    assert(attributes == SCE_GXM_MEMORY_ATTRIB_RW);
+    
     return SCE_OK;
 }
