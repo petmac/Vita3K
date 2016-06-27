5ed311a008c4f6465b23c1d7c841fd3470fcfc5a
Peter Mackay <mackay.pete+git@gmail.com>
2016-06-27 08:38:36 +0100

    First pass mouse touch support.

--- emulator/imports_touch.cpp
+++ emulator/imports_touch.cpp
@@ -1,5 +1,8 @@
 #include "import.h"
 
+#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_video.h>
+
 static uint64_t timestamp;
 
 // https://github.com/vitasdk/vita-headers/blob/master/include/psp2/touch.h
@@ -18,7 +21,7 @@ struct SceTouchData
     uint64_t timeStamp = 0;
     uint32_t status = 0;
     uint32_t reportNum = 0;
-    SceTouchReport report;
+    SceTouchReport report[8];
 };
 
 IMP_SIG(sceTouchPeek)
@@ -32,6 +35,25 @@ IMP_SIG(sceTouchPeek)
     
     memset(data, 0, sizeof(*data));
     data->timeStamp = timestamp++; // TODO Use the real time and units.
+    
+    int window_x = 0;
+    int window_y = 0;
+    const uint32_t buttons = SDL_GetMouseState(&window_x, &window_y);
+    
+    int window_w = 0;
+    int window_h = 0;
+    SDL_Window *const window = SDL_GetMouseFocus();
+    SDL_GetWindowSize(window, &window_w, &window_h);
+    
+    const float normalised_x = window_x / static_cast<float>(window_w);
+    const float normalised_y = window_y / static_cast<float>(window_h);
+    
+    if (buttons & SDL_BUTTON_LMASK)
+    {
+        data->report[data->reportNum].x = static_cast<uint16_t>(normalised_x * 1920);
+        data->report[data->reportNum].y = static_cast<uint16_t>(normalised_y * 1088);
+        ++data->reportNum;
+    }
     
     return SCE_OK;
 }
