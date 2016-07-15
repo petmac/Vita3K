faec92d15e2fb6c0602a398cfaf1324e8dd23684
Peter Mackay <mackay.pete+git@gmail.com>
2016-07-15 08:08:52 +0100

    uvl_exit

---
+++ emulator/imports_uvl.cpp
@@ -0,0 +1,14 @@
+#include "import.h"
+
+#include <unicorn/unicorn.h>
+
+IMP_SIG(uvl_exit)
+{
+    // http://yifanlu.github.io/UVLoader/group__uvloader.html#gab258eeb1f2b90ef9ca0cab0f9a3f2c39
+    const int status = r0;
+    (void)status;
+    uc_emu_stop(uc);
+    
+    return 0;
+}
+
