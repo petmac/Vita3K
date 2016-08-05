dc02a0d01970fd5c1564834a6a9f16aac3661967
Peter Mackay <mackay.pete+git@gmail.com>
2016-08-05 08:14:31 +0100

    Attempt to initialise GXM.

--- native-tool/src/main.cpp
+++ native-tool/src/main.cpp
@@ -1,26 +1,48 @@
 #include <psp2/kernel/processmgr.h>
-#include <sstream>
-#include <vector>
+#include <psp2/gxm.h>
 
-#include <cstdio>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	std::stringstream output;
-	std::vector<std::string> hello = { "Hello" };
-	hello.push_back(",");
-	hello.push_back(" C++ ");
-	hello.push_back("world!");
-	for (auto &s : hello) {
-		// std::cout does't work ATM :(
-		output << s;
-	}
-	output << std::endl;
-    FILE *fp = fopen("ux0:/data/hello.txt", "w");
-    if (fp)
+static void check(int result, const char *function, FILE *fp)
+{
+    if (result != 0)
     {
-        fprintf(fp, "%s\n", output.str().c_str());
+        fprintf(fp, "%s failed (%d, 0x%x).\n", function, result, result);
+        
         fclose(fp);
+        fp = nullptr;
+        
+        sceKernelExitProcess(1);
     }
-	sceKernelExitProcess(0);
+}
+
+static void display_callback(const void *data)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *fp = fopen("ux0:/data/hello.txt", "w");
+    if (fp == nullptr)
+    {
+        sceKernelExitProcess(1);
+        return 1;
+    }
+    
+    fprintf(fp, "Started.\n");
+    
+    SceGxmInitializeParams params = {};
+    params.displayQueueCallback = &display_callback;
+    params.displayQueueCallbackDataSize = 4;
+    params.displayQueueMaxPendingCount = 2;
+    params.parameterBufferSize = SCE_GXM_DEFAULT_PARAMETER_BUFFER_SIZE;
+    
+    check(sceGxmInitialize(&params), "sceGxmInitialize", fp);
+    
+    fprintf(fp, "Exiting normally.\n");
+    fclose(fp);
+    fp = nullptr;
+    
+    sceKernelExitProcess(0);
     return 0;
 }
