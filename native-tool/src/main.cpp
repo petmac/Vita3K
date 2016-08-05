b012cf40725cb47729b8f7030fff96232766269e
Peter Mackay <mackay.pete+git@gmail.com>
2016-08-05 13:25:45 +0100

    Dump clear_v.

--- native-tool/src/main.cpp
+++ native-tool/src/main.cpp
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+static const uint8_t clear_v_gxp[] =
+{
+#include "shaders/clear_v.h"
+};
+
 static void check(int result, const char *function, FILE *fp)
 {
     if (result != 0)
@@ -13,6 +18,43 @@ static void check(int result, const char *function, FILE *fp)
         fp = nullptr;
         
         sceKernelExitProcess(1);
+    }
+}
+
+static void dump(FILE *fp, const SceGxmProgram *program, const char *name)
+{
+    fprintf(fp, "Program %s at address %p:\n", name, program);
+    
+    unsigned int param_count = sceGxmProgramGetParameterCount(program);
+    fprintf(fp, "\t%u parameter(s)\n", param_count);
+    
+    for (unsigned int param_index = 0; param_index < param_count; ++param_index)
+    {
+        const SceGxmProgramParameter *const parameter = sceGxmProgramGetParameter(program, param_index);
+        const int offset = reinterpret_cast<const char *>(parameter) - reinterpret_cast<const char *>(program);
+        fprintf(fp, "\tParameter %u at address %p (offset = %d)\n", param_index, parameter, offset);
+        
+        const SceGxmParameterCategory category = sceGxmProgramParameterGetCategory(parameter);
+        const char *const name = sceGxmProgramParameterGetName(parameter);
+        const SceGxmParameterSemantic semantic = sceGxmProgramParameterGetSemantic(parameter);
+        const unsigned int semantic_index = sceGxmProgramParameterGetSemanticIndex(parameter);
+        const SceGxmParameterType type = sceGxmProgramParameterGetType(parameter);
+        const unsigned int component_count = sceGxmProgramParameterGetComponentCount(parameter);
+        const unsigned int array_size = sceGxmProgramParameterGetArraySize(parameter);
+        const unsigned int resource_index = sceGxmProgramParameterGetResourceIndex(parameter);
+        const unsigned int container_index = sceGxmProgramParameterGetContainerIndex(parameter);
+        const SceBool is_sampler_cube = sceGxmProgramParameterIsSamplerCube(parameter);
+        
+        fprintf(fp, "\t\tCategory = %u\n", category);
+        fprintf(fp, "\t\tName = %s\n", name);
+        fprintf(fp, "\t\tSemantic = %u\n", semantic);
+        fprintf(fp, "\t\tSemantic index = %u\n", semantic_index);
+        fprintf(fp, "\t\tType = %u\n", type);
+        fprintf(fp, "\t\tComponent count = %u\n", component_count);
+        fprintf(fp, "\t\tArray size = %u\n", array_size);
+        fprintf(fp, "\t\tResource index = %u\n", resource_index);
+        fprintf(fp, "\t\tContainer index = %u\n", container_index);
+        fprintf(fp, "\t\tIs sampler cube = %u\n", is_sampler_cube);
     }
 }
 
@@ -38,6 +80,7 @@ int main(int argc, char *argv[])
     params.parameterBufferSize = SCE_GXM_DEFAULT_PARAMETER_BUFFER_SIZE;
     
     check(sceGxmInitialize(&params), "sceGxmInitialize", fp);
+    dump(fp, reinterpret_cast<const SceGxmProgram *>(clear_v_gxp), "clear_v");
     
     fprintf(fp, "Exiting normally.\n");
     fclose(fp);
