ffa93f66c92ab3436807f6166f4fbf585e6fcf13
Peter Mackay <mackay.pete+git@gmail.com>
2016-08-10 08:15:30 +0100

    More verbose startup and error logging.

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -5,6 +5,7 @@
 #include <SDL2/SDL.h>
 
 #include <assert.h>
+#include <iostream>
 
 typedef std::unique_ptr<const void, void (*)(const void *)> SDLPtr;
 
@@ -27,20 +28,25 @@ static void term_sdl(const void *succeeded)
 
 int main(int argc, const char * argv[])
 {
+    std::cout << "Emulator starting..." << std::endl;
+    
     if (argc <= 2)
     {
+        std::cerr << "Incorrect args." << std::endl;
         return IncorrectArgs;
     }
     
     const SDLPtr sdl(reinterpret_cast<const void *>(SDL_Init(SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO) >= 0), term_sdl);
     if (!sdl)
     {
+        std::cerr << "SDL initialisation failed." << std::endl;
         return SDLInitFailed;
     }
     
     EmulatorState state;
     if (!init(&state))
     {
+        std::cerr << "Emulator initialisation failed." << std::endl;
         return EmulatorInitFailed;
     }
     
@@ -48,11 +54,15 @@ int main(int argc, const char * argv[])
     const char *const path = argv[1];
     if (!load(&module, &state.mem, path))
     {
+        std::cerr << "Failed to load module." << std::endl;
         return ModuleLoadFailed;
     }
     
+    std::cout << "Emulator initialised. Running main thread..." << std::endl;
+    
     if (!run_thread(&state, module.entry_point))
     {
+        std::cerr << "Failed to run main thread." << std::endl;
         return RunThreadFailed;
     }
     
